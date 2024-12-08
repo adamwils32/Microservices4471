be67e3b80e3df900a24517e2a16d57a9df243e90
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2024-12-08 00:46:16 -0500

    slight updates to app.py

--- compare_stocks/app.py
+++ compare_stocks/app.py
@@ -16,6 +16,9 @@ def lambda_handler(event, context):
     params = event['queryStringParameters']
     ticker1 = params.get('ticker1')
     ticker2 = params.get('ticker2')
+    ticker3 = params.get('ticker3')
+    ticker4 = params.get('ticker4')
+    ticker5 = params.get('ticker5')
 
     if not ticker1 or not ticker2:
         return {
@@ -27,6 +30,9 @@ def lambda_handler(event, context):
     try:
         stock1 = get_stock_from_db(ticker1)
         stock2 = get_stock_from_db(ticker2)
+        stock3 = get_stock_from_db(ticker3)
+        stock4 = get_stock_from_db(ticker4)
+        stock5 = get_stock_from_db(ticker5)
     except ClientError as e:
         print(e.response['Error']['Message'])
         return {
@@ -35,21 +41,30 @@ def lambda_handler(event, context):
         }
 
     #check if both stocks were found (efficient)
-    if stock1 or stock2:
+    if stock1 or stock2 or stock3 or stock4 or stock5:
         not_found_tickers = []
         if stock1 is None:
             not_found_tickers.append(ticker1)
         if stock2 is None:
             not_found_tickers.append(ticker2)
+        if stock3 is None:
+            not_found_tickers.append(ticker3)
+        if stock4 is None:
+            not_found_tickers.append(ticker4)
+        if stock5 is None:
+            not_found_tickers.append(ticker5)
         return {
             'statusCode': 404,
             'body': json.dumps({'message': f'Stocks not found: {", ".join(not_found_tickers)}'})
         }
         print(f"Stock1: {stock1}")
         print(f"Stock2: {stock2}")
+        print(f"Stock3: {stock3}")
+        print(f"Stock4: {stock4}")
+        print(f"Stock5: {stock5}")
 
     # Compare the stock data
-    comparison_result = compare_stocks(stock1, stock2)
+    comparison_result = compare_stocks(stock1, stock2, stock3, stock4, stock5)
 
     return {
         'statusCode': 200,
@@ -70,19 +85,24 @@ def get_stock_from_db(ticker):
     return response.get('Item')
 
 # Compare two stock data dictionaries
-def compare_stocks(stock1, stock2):
+def compare_stocks(stock1, stock2, stock3, stock4, stock5):
     # Define the fields to compare
     fields_to_compare = ['price', 'volume', 'change_percent', 'previous_close', 'change', 'open', 'high', 'low', 'close', 'adj close', 'average close price']
 
     comparison = {
         'stock1': {'ticker': stock1['ticker']},
         'stock2': {'ticker': stock2['ticker']},
-        'comparisons': {}
-    }
+        'stock3': {'ticker': stock3['ticker']},
+        'stock4': {'ticker': stock4['ticker']},
+        'stock5': {'ticker': stock5['ticker']}
+        }
 
     for field in fields_to_compare:
         value1 = stock1.get(field)
         value2 = stock2.get(field)
+        value3 = stock3.get(field)
+        value4 = stock4.get(field)
+        value5 = stock5.get(field)
 
         if value1 and value2:
             if value1 is None and value2 is None:
@@ -91,14 +111,29 @@ def compare_stocks(stock1, stock2):
                     value1 = float(value1)
                 if isinstance(value2, Decimal):
                     value2 = float(value2)
+                if isinstance(value3, Decimal):
+                    value3 = float(value3)
+                if isinstance(value4, Decimal):
+                    value4 = float(value4)
+                if isinstance(value5, Decimal):
+                    value5 = float(value5)
 
             comparison['stock1'][field] = value1
             comparison['stock2'][field] = value2
+            comparison['stock3'][field] = value3
+            comparison['stock4'][field] = value4
+            comparison['stock5'][field] = value5
 
             if value1 > value2:
                 result = f'{stock1["ticker"]} has higher {field}'
             elif value1 < value2:
                 result = f'{stock2["ticker"]} has higher {field}'
+            elif value2 > value3:
+                result = f'{stock3["ticker"]} has higher {field}'
+            elif value2 < value3:
+                result = f'{stock4["ticker"]} has higher {field}'
+            elif value3 > value4:
+                result = f'{stock5["ticker"]} has higher {field}'
             else:
                 result = f'Both stocks have equal {field}'
 
